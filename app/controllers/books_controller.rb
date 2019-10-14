de2b15c1aa38bf764e600dfb44758d1895bb94c9
Alper-Enes-DURU <enesduru@gmail.com>
2019-10-14 18:17:30 +0000

    Quiz finished. Now user can upload new book informations and see them on the main page.

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,22 @@
+class BooksController < ApplicationController
+
+  def index
+    @books = Book.all
+  end
+
+  def new
+    @book = Book.new
+  end
+
+  def create
+    Book.create(book_params)
+    redirect_to root_path
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:name, :description, :page)
+  end
+
+end
